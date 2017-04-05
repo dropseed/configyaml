a1d22a0be40d74fb2e8f0915020edaec6ba68979
Paul Ortman <paul.ortman@gmail.com>
2017-04-05 13:53:15 -0500

    update __version__ to get from setup.py

--- configyaml/__init__.py
+++ configyaml/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+import pkg_resources
+
 
 __author__ = """Dropseed"""
 __email__ = 'python@dropseed.io'
-__version__ = '0.1.0'
+__version__ = pkg_resources.get_distribution("configyaml").version
